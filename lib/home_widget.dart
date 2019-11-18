0b8750903ef978a412150391cd4d32abe85c18d2
Daan Vissers <daanvissers81@gmail.com>
2019-11-18 20:12:48 +0100

    Request token by authenticating

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,13 +1,54 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 
-class HomeWidget extends StatelessWidget {
+class HomeWidget extends StatefulWidget {
+  HomeWidgetState createState() => HomeWidgetState();
+}
 
-  HomeWidget();
+class HomeWidgetState extends State<HomeWidget> {
+  String _text = "Please authenticate first!";
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("This is your profile. Stay tuned!"),
-    );
+        child: new Column(
+      children: <Widget>[
+        new Text(_text),
+        new RaisedButton(child: Text("Log in"), onPressed: _requestToken),
+      ],
+    ));
+  }
+
+  void _requestToken() async {
+    final BASE_URL = "http://www.last.fm/api/auth/?api_key=";
+
+    // First load the config.json text file.
+    // Then serialize the JSON manually using dart:convert to get the key
+    final jsonString = await rootBundle.loadString('assets/config.json');
+    Map<String, dynamic> key = jsonDecode(jsonString);
+    final API_KEY = key['API key'];
+
+    // This is the custom callback-URL. This makes the API redirect to
+    // a custom Android Activity as found in the AndroidManifest.xml
+    final CALLBACK = "&cb=lastfmbrowser://authenticate";
+
+    // Present the login dialog to the user
+    final result = await FlutterWebAuth.authenticate(
+        url: BASE_URL + API_KEY + CALLBACK, callbackUrlScheme: "lastfmbrowser");
+
+    // Extract token from resulting url.
+    // Authentication tokens are user and API account specific.
+    // They are valid for 60 minutes from the moment they are granted.
+    String token = Uri.parse(result).queryParameters['token'];
+
+    log(token);
+
+    setState(() {
+      _text = "Succesfully authenticated!";
+    });
   }
 }
